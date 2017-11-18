b85ce4d1101700966d82d1b4eabfb303558c2000
jntp <justin.jntprules@gmail.com>
2017-11-17 22:58:00 -0800

    updated txt2netcdf.py

--- stars/txt2netcdf.py
+++ stars/txt2netcdf.py
@@ -9,9 +9,26 @@ mydata = Dataset('scat_data.nc', 'w', format='NETCDF4')
 mydata.description = 'Star Catalog'
 
 # dimensions
-mydata.createDimension(
+mydata.createDimension('right_ascension', )
+mydata.createDimension('declination', )
 
+ra = mydata.createVariable('right_ascension', 'f4', 'declination')
+ra.long_name = "right ascension"
+ra.units = "hours:minutes:seconds"
+ra.standard_name = "right ascension"
 
+dec = mydata.createVariable('declination', 'f4', 'right_ascension')
+dec.long_name = "declination"
+dec.units = "degrees:minutes:seconds"
+dec.standard_name = "declination"
+
+mag = mydata.createVariable('magnitude', 'f4', ( ), fill_value= )
+mag.long_name = "apparent visual magnitude"
+# apparent magnitude is unitless, so no need to denote units
+mag.standard_name = "apparent magnitude"
+
+type = mydata.createVariable('type', 'f4', ( ), fill_value= )
+type.long_name = 
 
 
 
