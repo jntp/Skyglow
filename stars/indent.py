3a08c2efc772e258779b728c3cd0bb735736af89
jntp <justin.jntprules@gmail.com>
2017-11-24 23:11:30 -0800

    debugging the indent.py file

--- stars/indent.py
+++ stars/indent.py
@@ -2,15 +2,14 @@
 # This script reformats scat_data.txt by converting single whitespace and triple whitespace "delimiters" to double whitespaces. The goal is for
 # netcdf4-python to properly read, parse, and convert scat_data.txt to a netcdf4 file.
 
-with open("scat_data.txt") as file:
+with open("scat_data.txt", "r+") as file:
   data = file.readlines()
-  i = 0 # for keeping track of the indices during the for loop
 
-  for thestring in data:
-    lastindex = len(thestring) - 1 
+  for i, thestring in enumerate(data):
+    lastindex = len(thestring)
 
     # Delete the double space at the beginning of each string
-    data[i].lstrip() 
+    thestring = thestring[2:lastindex]
   
     # Find single and triple spaces and turn them into double spaces
     a = thestring.find(' ') # find the lowest index of the whitespace character
@@ -19,23 +18,36 @@ with open("scat_data.txt") as file:
     if a == ' ' and b != ' ': # For single spaces
       # Create two separate strings for concatenation
       stringone = thestring[0:a]
+      print(stringone)
       stringtwo = thestring[b:lastindex]
 
       thestring = stringone + " " + stringtwo # insert whitespace between strings
 
     # Find and delete the triple space
     c = thestring.find('   ')
-    d = c + 2 
-    lastindex = len(thestring) # update the lastindex variable
 
-    stringone = thestring[0:c]
-    stringtwo = thestring[d:lastindex]
+    if c != -1: # check if triple space exists in string
+      d = c + 2 
+      lastindex = len(thestring) # update the lastindex variable
 
-    thestring = stringone + stringtwo # simply concatenate the two strings, skipping the index between c and d
-    data[i] = thestring # end product
+      stringone = thestring[0:c]
+      stringtwo = thestring[d:lastindex]
 
-    file.write(thestring) # write to file
+      thestring = stringone + " " + stringtwo # concatenate the two strings, inserting only one whitespace in between
+    # else:
+      # thestring = thestring + "\n" # only concatenate the newline character
+    
+    # print(thestring)
+
+    data[i] = thestring
+  
+  print(data[0])
+    
+  file.writelines(data) # write to file
 
 file.close()
 
-# You left off at addressing the file.write error
+
+
+# You left off at addressing the file.write error (should not use "r+")
+# You still need to figure out why the single character space isn't becoming double
