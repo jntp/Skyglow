f078c573f383de7e6ed941e46ae19f1d096f2652
jntp <justin.jntprules@gmail.com>
2017-11-27 23:16:59 -0800

    now adding ra and dec hourangle to degrees conversion to indent.py

--- stars/indent.py
+++ stars/indent.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # This script reformats scat_data.txt by converting single whitespace and triple whitespace "delimiters" to double whitespaces. The goal is for
 # netcdf4-python to properly read, parse, and convert scat_data.txt to a netcdf4 file.
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
 
 # Read in data from file-to-be-reformatted
 with open("scat_data.txt", "r") as file:
@@ -34,10 +37,22 @@ with open("scat_data.txt", "w") as file:
       stringtwo = thestring[d:lastindex]
 
       thestring = stringone + " " + stringtwo # concatenate the two strings, inserting only one whitespace in between
-   
+  
+    # Convert right ascension and declination to degrees
+    rascension = thestring[7:19]
+    declination = thestring[21:33]
+    lastindex = len(thestring) # update the lastindex variable again
+    coords = SkyCoord(ra=rascension*u.hourangle, dec=declination*u.hourangle) # Create coord object to allow access to components
+    
+    # Replace ra and dec in text file with the newly converted units
+    thestring = thestring[0:7] + coords.ra.degree + thestring[19:21] + coords.dec.degree + thestring[33:lastindex]
+
     # Write newly concatenated string to data and file
     data[i] = thestring
     file.write(data[i])
 
 file.close()
+
+
+# You left off at debugging the script. Get right ascension and decination converted!
 
