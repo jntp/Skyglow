e9bb8e2a9167627c8320a86173e660fcaf4c7529
jntp <justin.jntprules@gmail.com>
2017-11-28 22:48:53 -0800

    indent.py now converts ra and dec to decimal form

--- stars/indent.py
+++ stars/indent.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3.6
 # This script reformats scat_data.txt by converting single whitespace and triple whitespace "delimiters" to double whitespaces. The goal is for
 # netcdf4-python to properly read, parse, and convert scat_data.txt to a netcdf4 file.
-from astropy import units as u
-from astropy.coordinates import SkyCoord
 
 
 # Read in data from file-to-be-reformatted
@@ -38,14 +36,38 @@ with open("scat_data.txt", "w") as file:
 
       thestring = stringone + " " + stringtwo # concatenate the two strings, inserting only one whitespace in between
   
-    # Convert right ascension and declination to degrees
-    rascension = thestring[7:19]
-    declination = thestring[21:33]
+    # Convert right ascension to decimal form
+    ra = thestring[8:20] # get right ascension in string form
+
+    # Parse the string and convert to float to allow for mathematical operation
+    ra_hr = float(ra[0:2])
+    ra_min = float(ra[3:5])
+    ra_sec = float(ra[6:12])
+
+    ra_min = ra_min / 60    # Convert minutes to hour form
+    ra_sec = ra_sec / 3600  # Convert seconds to hour form
+
+    ra_new = ra_hr + ra_min + ra_sec # add to obtain the decimal form
+    ra = str(ra_new)
+
+    # Convert declination to decimal form
+    dec = thestring[22:34]
+
+    dec_sign = dec[0]
+    dec_deg = float(dec[1:3])
+    dec_arcmin = float(dec[4:6])
+    dec_arcsec = float(dec[7:12])
+
+    dec_arcmin = dec_arcmin / 60    # Convert arcminutes to degree form
+    dec_arcsec = dec_arcsec / 3600  # Convert arcseconds to degree form
+
+    dec_new = dec_deg + dec_arcmin + dec_arcsec # add to obtain the decimal form
+    dec = dec_sign + str(dec_new) # concatenate the new string number with the sign
+
     lastindex = len(thestring) # update the lastindex variable again
-    coords = SkyCoord(ra=rascension*u.hourangle, dec=declination*u.hourangle) # Create coord object to allow access to components
     
     # Replace ra and dec in text file with the newly converted units
-    thestring = thestring[0:7] + coords.ra.degree + thestring[19:21] + coords.dec.degree + thestring[33:lastindex]
+    thestring = thestring[0:7] + " " + ra[0:7] + thestring[19:21] + " " + dec[0:7] + thestring[33:lastindex]
 
     # Write newly concatenated string to data and file
     data[i] = thestring
