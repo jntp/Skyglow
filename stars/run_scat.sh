49819a46bc418127b8243616c10a792ed63ba7fd
jntp <justin.jntprules@gmail.com>
2018-03-02 23:18:06 -0800

    started writing the bash script run_scat.sh

--- stars/run_scat.sh
+++ stars/run_scat.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
 
+# Number of lines of radec_values from the wc command will be off; correct by adding 1
+strOfRaDec=$( wc -l < radec_values.txt )
+numOfRaDec=$(( $strOfRaDec + 1))
 
-# You left off at reading the BASH tutorial
+# Loop through every centered ra and dec value and run the scat command to obtain nearby stars
+i=1
+while [ $i -le $numOfRaDec ]
+do
+  radec=$( cat radec_values.txt | sed -n "$i"p )
+  
+  scat -c sao -h -m 6.0 -r 1800 $radec > nearby_stars.txt # save in text file so that wc can work
+  nbr_num=$( wc -l < nearby_stars.txt )
+
+  # Stuff each nbr_num into text file (append probably)
+
+  ((i++))
+done 
+
+
+
+
+# You left off at trying to figure out how to stuff the wc output into the scat_data.txt file
 
 # INSTRUCTIONS:
 # Run scat -c sao -h -m 6.0 -r 18000 $ra $dec | m
