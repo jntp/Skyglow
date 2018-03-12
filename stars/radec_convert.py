53dc3db743be4a60c1e54399de471461432e203f
jntp <justin.jntprules@gmail.com>
2018-03-12 00:18:22 -0700

    wrote nbr_num into txt file and started writing python script to convert centered ra and dec values to decimal form

---
+++ stars/radec_convert.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.6
+
+
+# Read in data from file-to-be-formatted
+with open("radec_values.txt", "r") as file:
+  data = file.readlines()
+file.close()
+
+with open("radec_decimals.txt", "w") as file:
+
+file.close()
+
+
+
+# You left off at converting radec to decimal form
--- stars/run_scat.sh
+++ stars/run_scat.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Retrieves right ascension and declination coordinates from radec_values.txt and runs the scat command for each coordinate. Stores the output into
+# a separate file and counts the number of stars. Stores the number into nbr_num.txt.
 
 
 # Number of lines of radec_values from the wc command will be off; correct by adding 1
@@ -15,32 +17,17 @@ do
   scat -c sao -h -m 6.0 -r 18000 $radec > nearby_stars.txt # save in text file so that wc can work
   
   nbr_num=$( wc -l < nearby_stars.txt ) # read nearby_stars.txt and display the number of lines
-  # nbr_num=$(($nbr_num - 6)) # Subtract 6 since those six lines are part of a useless header
+  nbr_num=$(($nbr_num - 6)) # Subtract 6 since those six lines are part of a useless header
 
   # Take into account centered ra/dec values with no stars nearby
   # In these cases, nbr_num will equal -2. Change that to 0
-  if [ $nbr_num -eq -2] 
+  if [ $nbr_num -eq -2 ] 
   then
     nbr_num=0
   fi
 
-  echo $nbr_num
-  
-  # Stuff each nbr_num into text file (append probably)
-  sed "$i"i $nbr_num >> radec_values.txt
+  # Stuff each nbr_num into text file
+  echo $nbr_num >> nbr_num.txt
 
   ((i++))
 done 
-
-
-
-# How the hell do you append this shit to radec_values.txt?!?!
-# You left off at trying to figure out how to stuff the wc output into the scat_data.txt file
-
-# INSTRUCTIONS:
-# Run scat -c sao -h -m 6.0 -r 18000 $ra $dec | m
-# where 18000 = 5 deg arcseconds radius and each $ra and $dec is a line from radec_values.txt
-# Store output in file?
-# Run wc -l file.txt to count number of lines of output
-
-
