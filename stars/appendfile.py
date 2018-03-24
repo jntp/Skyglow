e37f5ae3f06cca8573b3c3898e9a4b1d5f49a032
jntp <justin.jntprules@gmail.com>
2018-03-23 23:59:15 -0700

    bleh modifying radec_decimals.txt thru append.py again since ra needs to be converted to degrees longitude and StarDensity needs to be # of stars per sterradian

--- stars/appendfile.py
+++ stars/appendfile.py
@@ -13,15 +13,22 @@ with open("radec_decimals.txt", "r") as file:
 file.close()
 
 # Write nbr_num data to radec_decimals.txt
-with open("radec_decimals.txt", "w") as file:
+with open("radec_decimals.txt", "w") as file:  
+  # Append nbr_num data to text file
   for i, line in enumerate(coords):
     # Strip off newlines off line and nbr_num
     line = line.strip('\n')
     nbr_num[i] = nbr_num[i].strip('\n')
     
+    # Convert right ascension to degrees longitude
+    setValues = line.split(" ")
+    print(setValues[0])
+
     line = line + " " + nbr_num[i] + "\n" 
 
     # Write newly concatenated string to coords and file
     coords[i] = line
     file.write(coords[i])
 file.close()
+
+# You left off at converting ra to degrees longitude
